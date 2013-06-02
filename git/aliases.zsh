4cdcccfd2814eda24e3744546b2570f22aea45a1
Joe Stanton <joe.stanton@red-badger.com>
2013-06-02 23:25:49 +0100

    Added unstage and other aliases for git.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,9 +7,13 @@ then
 fi
 
 # The rest of my fun git aliases
-alias gl='git pull --prune'
+alias gf='git fetch'
+alias gpu='git pull --prune'
+alias greb='git pull --rebase'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
+alias ga='git add'
+alias gu='git unstage'
 alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
@@ -17,3 +21,6 @@ alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+
+alias grh='git reset HEAD --soft'
+alias grhh='git reset HEAD --hard'
